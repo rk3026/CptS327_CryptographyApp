4bd5780b03ba56b40bbda23c86e9a559a6ff6e7f
Ross <rosskugler@gmail.com>
2024-10-01 00:45:32 -0700

    Added base project, some encryption applications

---
+++ Project/CryptographyApp/CryptographyApp/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿using System.Security.Cryptography;
+
+namespace CryptographyApp
+{
+    partial class Form1
+    {
+        // Declare CspParameters and RsaCryptoServiceProvider
+        // objects with global scope of your Form class.
+        readonly CspParameters _cspp = new CspParameters();
+        RSACryptoServiceProvider _rsa;
+
+        // Path variables for source, encryption, and
+        // decryption folders. Must end with a backslash.
+        const string EncrFolder = @"c:\Encrypt\";
+        const string DecrFolder = @"c:\Decrypt\";
+        const string SrcFolder = @"c:\docs\";
+
+        // Public key file
+        const string PubKeyFile = @"c:\encrypt\rsaPublicKey.txt";
+
+        // Key container name for
+        // private/public key value pair.
+        const string KeyName = "Key01";
+
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            buttonEncryptFile = new Button();
+            buttonDecryptFile = new Button();
+            buttonCreateAsmKeys = new Button();
+            buttonExportPublicKey = new Button();
+            buttonImportPublicKey = new Button();
+            buttonGetPrivateKey = new Button();
+            label1 = new Label();
+            _encryptOpenFileDialog = new OpenFileDialog();
+            _decryptOpenFileDialog = new OpenFileDialog();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label4 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            tabPage3 = new TabPage();
+            tabPage2 = new TabPage();
+            label3 = new Label();
+            richTextBox1 = new RichTextBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // buttonEncryptFile
+            // 
+            buttonEncryptFile.Location = new Point(57, 122);
+            buttonEncryptFile.Name = "buttonEncryptFile";
+            buttonEncryptFile.Size = new Size(154, 29);
+            buttonEncryptFile.TabIndex = 0;
+            buttonEncryptFile.Text = "Encrypt File\t";
+            buttonEncryptFile.UseVisualStyleBackColor = true;
+            buttonEncryptFile.Click += buttonEncryptFile_Click;
+            // 
+            // buttonDecryptFile
+            // 
+            buttonDecryptFile.Location = new Point(57, 170);
+            buttonDecryptFile.Name = "buttonDecryptFile";
+            buttonDecryptFile.Size = new Size(154, 29);
+            buttonDecryptFile.TabIndex = 1;
+            buttonDecryptFile.Text = "Decrypt File";
+            buttonDecryptFile.UseVisualStyleBackColor = true;
+            buttonDecryptFile.Click += buttonDecryptFile_Click;
+            // 
+            // buttonCreateAsmKeys
+            // 
+            buttonCreateAsmKeys.Location = new Point(57, 24);
+            buttonCreateAsmKeys.Name = "buttonCreateAsmKeys";
+            buttonCreateAsmKeys.Size = new Size(154, 29);
+            buttonCreateAsmKeys.TabIndex = 2;
+            buttonCreateAsmKeys.Text = "Create Asm Keys";
+            buttonCreateAsmKeys.UseVisualStyleBackColor = true;
+            buttonCreateAsmKeys.Click += buttonCreateAsmKeys_Click;
+            // 
+            // buttonExportPublicKey
+            // 
+            buttonExportPublicKey.Location = new Point(57, 76);
+            buttonExportPublicKey.Name = "buttonExportPublicKey";
+            buttonExportPublicKey.Size = new Size(154, 29);
+            buttonExportPublicKey.TabIndex = 3;
+            buttonExportPublicKey.Text = "Export Public Key";
+            buttonExportPublicKey.UseVisualStyleBackColor = true;
+            buttonExportPublicKey.Click += buttonExportPublicKey_Click;
+            // 
+            // buttonImportPublicKey
+            // 
+            buttonImportPublicKey.Location = new Point(310, 170);
+            buttonImportPublicKey.Name = "buttonImportPublicKey";
+            buttonImportPublicKey.Size = new Size(142, 29);
+            buttonImportPublicKey.TabIndex = 4;
+            buttonImportPublicKey.Text = "Import Public Key";
+            buttonImportPublicKey.UseVisualStyleBackColor = true;
+            buttonImportPublicKey.Click += buttonImportPublicKey_Click;
+            // 
+            // buttonGetPrivateKey
+            // 
+            buttonGetPrivateKey.Location = new Point(314, 240);
+            buttonGetPrivateKey.Name = "buttonGetPrivateKey";
+            buttonGetPrivateKey.Size = new Size(133, 29);
+            buttonGetPrivateKey.TabIndex = 5;
+            buttonGetPrivateKey.Text = "Get Private Key";
+            buttonGetPrivateKey.UseVisualStyleBackColor = true;
+            buttonGetPrivateKey.Click += buttonGetPrivateKey_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(326, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Key not set";
+            // 
+            // _encryptOpenFileDialog
+            // 
+            _encryptOpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // _decryptOpenFileDialog
+            // 
+            _decryptOpenFileDialog.FileName = "openFileDialog2";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 450);
+            tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(comboBox1);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 417);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Text Encryption";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(270, 72);
+            label4.Name = "label4";
+            label4.Size = new Size(143, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Select an Algorithm:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(419, 68);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(33, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Enter Text:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(116, 69);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(4, 29);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(792, 417);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Hashing";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(richTextBox1);
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(buttonExportPublicKey);
+            tabPage2.Controls.Add(buttonImportPublicKey);
+            tabPage2.Controls.Add(buttonCreateAsmKeys);
+            tabPage2.Controls.Add(buttonGetPrivateKey);
+            tabPage2.Controls.Add(buttonDecryptFile);
+            tabPage2.Controls.Add(buttonEncryptFile);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 417);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "File Encrpytion";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(232, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(88, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Current Key:";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(446, 6);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(338, 297);
+            richTextBox1.TabIndex = 8;
+            richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button buttonEncryptFile;
+        private Button buttonDecryptFile;
+        private Button buttonCreateAsmKeys;
+        private Button buttonExportPublicKey;
+        private Button buttonImportPublicKey;
+        private Button buttonGetPrivateKey;
+        private Label label1;
+        private OpenFileDialog _encryptOpenFileDialog;
+        private OpenFileDialog _decryptOpenFileDialog;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label4;
+        private ComboBox comboBox1;
+        private TabPage tabPage3;
+        private Label label3;
+        private RichTextBox richTextBox1;
+    }
+}
